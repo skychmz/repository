4f9fd8929502bbf5ab8d74b1783a14078b9ad557
skychmz <282118010@qq.com>
2018-07-11 18:33:06 +0800

    ''

--- src/main/java/com/eshop/controller/portal/UserController.java
+++ src/main/java/com/eshop/controller/portal/UserController.java
@@ -3,6 +3,7 @@ package com.eshop.controller.portal;
 import com.eshop.common.*;
 import com.eshop.pojo.User;
 import com.eshop.service.IUserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,7 @@ public class UserController {
         //session.removeAttribute(Const.CURRENT_USER);
         String loginToken = CookieUtil.readLoginToken(httpServletRequest);
         CookieUtil.delLoginToken(httpServletRequest,httpServletResponse);
+        RedisPoolUtil.del(loginToken);
         return ServerResponse.createBySuccess();
     }
 
@@ -54,8 +56,13 @@ public class UserController {
 
     @RequestMapping(value = "get_user_info.do",method= RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session){
-        User user=(User)session.getAttribute(Const.CURRENT_USER);
+    public ServerResponse<User> getUserInfo(HttpServletRequest httpServletRequest){
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if(StringUtils.isEmpty(loginToken)){
+            return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+        }
+        String userJsonStr = RedisPoolUtil.get(loginToken);
+        User user = JsonUtil.string2Obj(userJsonStr,User.class);
         if(user!=null){
             return ServerResponse.createBySuccess(user);
         }
