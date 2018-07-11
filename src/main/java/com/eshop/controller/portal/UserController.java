df2665008c188ad725ad585742ef56f1f3552612
skychmz <282118010@qq.com>
2018-07-11 18:10:08 +0800

    ''

--- src/main/java/com/eshop/controller/portal/UserController.java
+++ src/main/java/com/eshop/controller/portal/UserController.java
@@ -25,6 +25,7 @@ public class UserController {
         ServerResponse<User> response=iUserService.login(username,password);
         if (response.isSuccess()){
             //session.setAttribute(Const.CURRENT_USER,response.getData());
+            System.out.println();
             CookieUtil.writeLoginToken(httpServletResponse,session.getId());
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
